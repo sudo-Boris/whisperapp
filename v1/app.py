5e505abde867bba21bb7d676d243e320e32ab0b0
sudo-Boris <boris.meinardus00@gmail.com>
2022-10-12 11:51:47 +0200

    Update code for v1.

--- v1/app.py
+++ v1/app.py
@@ -8,7 +8,7 @@ import whisper
 
 model = whisper.load_model("small")
 
-# Create the app
+# create the app
 app = tk.Tk()
 app.geometry("532x632")
 app.title("Voice to text")
@@ -18,64 +18,6 @@ main_label = ctk.CTkLabel(
     height=512, width=512, text_color="black", text_font=("Roboto Medium", -16)
 )
 main_label.place(x=10, y=110)
-
-language_input = ctk.CTkEntry(
-    height=40,
-    width=512,
-    text_font=("Arial", 20),
-    text_color="black",
-    fg_color="white",
-    placeholder_text="Enter language to support model.",
-)
-language_input.place(x=10, y=10)
-
-
-def transcribe():
-    main_label.configure(text="Transcribing...")
-    audio = "/Users/borismeinardus/personal_projects/whisperapp/v1/my_Audio_file.flac"
-    # audio = whisper.load_audio(audio)
-    # audio = whisper.pad_or_trim(audio)
-
-    # mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    if language_input.get():
-        language = language_input.get()
-        print(language)
-    else:
-        # _, probs = model.detect_language(mel)
-        language = None
-
-    # options = whisper.DecodingOptions(fp16=False, language=language)
-    options = {"fp16": False, "language": language, "task": "transcribe"}
-    result = model.transcribe(audio, **options)
-
-    print(result["text"])
-    main_label.configure(text=result["text"])
-    return result["text"]
-
-
-def translate():
-    main_label.configure(text="Translating...")
-    audio = "/Users/borismeinardus/personal_projects/whisperapp/v1/my_Audio_file.flac"
-    # audio = whisper.load_audio(audio)
-    # audio = whisper.pad_or_trim(audio)
-
-    # mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-    if language_input.get():
-        language = language_input.get()
-        print(language)
-    else:
-        # _, probs = model.detect_language(mel)
-        language = None
-
-    # options = whisper.DecodingOptions(fp16=False, language=language)
-    options = {"fp16": False, "language": language, "task": "translate"}
-    result = model.transcribe(audio, **options)["text"]
-
-    print(result)
-    main_label.configure(text=result)
-    return result
 
 
 def voice_rec():
@@ -88,25 +30,45 @@ def voice_rec():
     sd.wait()
 
     # Save as FLAC file at correct sampling rate
-    sf.write("v1/my_Audio_file.flac", myrecording, fs)
+    sf.write("my_Audio_file.flac", myrecording, fs)
     main_label.configure(text="Recording done")
 
 
-record = ctk.CTkButton(
+def transcribe():
+    audio = "my_Audio_file.flac"
+
+    options = {"fp16": False, "language": "Bulgarian", "task": "transcribe"}
+    results = model.transcribe(audio, **options)
+
+    print(results["text"])
+    main_label.configure(text=results["text"])
+
+
+def translate():
+    audio = "my_Audio_file.flac"
+
+    options = {"fp16": False, "language": "Bulgarian", "task": "translate"}
+    results = model.transcribe(audio, **options)
+
+    print(results["text"])
+    main_label.configure(text=results["text"])
+
+
+recordButton = ctk.CTkButton(
     height=40,
     width=120,
-    text_font=("Arial", 20),
+    text_font=("Roboto Medium", 20),
     text_color="white",
     fg_color=("white", "gray38"),
     command=voice_rec,
 )
-record.configure(text="Record")
-record.place(x=206, y=60)
+recordButton.configure(text="Record")
+recordButton.place(x=206, y=60)
 
 transcribeButton = ctk.CTkButton(
     height=40,
     width=120,
-    text_font=("Arial", 20),
+    text_font=("Roboto Medium", 20),
     text_color="white",
     fg_color=("white", "gray38"),
     command=transcribe,
@@ -117,7 +79,7 @@ transcribeButton.place(x=106, y=150)
 translateButton = ctk.CTkButton(
     height=40,
     width=120,
-    text_font=("Arial", 20),
+    text_font=("Roboto Medium", 20),
     text_color="white",
     fg_color=("white", "gray38"),
     command=translate,
@@ -125,4 +87,6 @@ translateButton = ctk.CTkButton(
 translateButton.configure(text="Translate")
 translateButton.place(x=306, y=150)
 
+
+# run app
 app.mainloop()
